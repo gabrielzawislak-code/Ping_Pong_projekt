13cb019acc2fd6fd5534277bd7c251c161e89dbc
Mateusz Zybura <matzyb99@gmail.com>
2026-08-31 20:55:18 +0200

    Dodanie gotowego raportu i checklisty w PDF, usuniecie szkicu raportu
    - doc/raport.pdf - finalny raport (wczesniej byl tylko szkic raport_draft.md)
    - doc/checklist.pdf - wypelniona checklista w wymaganym formacie PDF
    - usuniety doc/raport_draft.md, zastapiony przez raport.pdf

---
+++ sim/build/xsim.dir/game_fsm_tb/xsim_script.tcl
@@ -0,0 +1,4 @@
+set_param project.enableReportConfiguration 0
+load_feature core
+current_fileset
+xsim {game_fsm_tb} -autoloadwcfg -tclbatch {/home/student/gzawislak/uec2-projekt/uec2-projekt/tools/sim_cmd.tcl}
